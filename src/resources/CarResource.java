61d8cad5ab1b814b52c950701feb89f128454c1d
JavaScript0205 <javascript0222@gmail.com>
2024-01-09 17:53:08 +0500

    resource successfully ended

--- src/resources/CarResource.java
+++ src/resources/CarResource.java
@@ -1,4 +1,27 @@
 package resources;
 
-public class CarResource {
+import beans.APIResponse;
+import beans.CarBean;
+
+public class CarResource implements BaseCRUDResource<CarBean> {
+    @Override
+    public APIResponse add(CarBean bean) {
+
+        return null;
+    }
+
+    @Override
+    public APIResponse get(Integer ID) {
+        return null;
+    }
+
+    @Override
+    public APIResponse update(CarBean newBean) {
+        return null;
+    }
+
+    @Override
+    public APIResponse delete(Integer ID) {
+        return null;
+    }
 }
--- src/resources/UserResource.java
+++ src/resources/UserResource.java
@@ -1,4 +1,36 @@
 package resources;
 
-public class UserResource {
+import beans.APIResponse;
+import beans.UserBean;
+import db.DB;
+
+public class UserResource implements BaseCRUDResource<UserBean> {
+    @Override
+    public APIResponse add(UserBean bean) {
+        UserBean newUser = DB.addUser(bean);
+        return newUser == null ? new APIResponse(400, "User already exists", null) : new APIResponse(200, "Successfully added", newUser);
+    }
+
+    @Override
+    public APIResponse get(Integer ID) {
+        UserBean user = DB.getUser(ID);
+        return user == null ? new APIResponse(400, "User does not exist!", null) : new APIResponse(200, "Successfully", user);
+    }
+
+    @Override
+    public APIResponse update(UserBean newBean) {
+        boolean ok = DB.updateUser(newBean);
+        if (!ok) {
+            System.out.println("User update failed!");
+        } else {
+            System.out.println(newBean);
+        }
+        return !ok ? new APIResponse(400, "User updated failed!", null) : new APIResponse(200, "User successfully updated", newBean);
+    }
+
+    @Override
+    public APIResponse delete(Integer ID) {
+        boolean ok = DB.deleteUser(ID);
+        return !ok ? new APIResponse(400, "User delete failed!", false) : new APIResponse(200, "User has been successfully deleted!", true);
+    }
 }
